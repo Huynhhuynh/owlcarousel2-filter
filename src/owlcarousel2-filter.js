422e927e88d4904b64be0f85982e377012735124
user <user@users-iMac.local>
2019-02-24 14:38:52 +0700

    first commit

---
+++ src/owlcarousel2-filter.js
@@ -0,0 +1,38 @@
+/**
+ * OwlCarousel 2 - Filter
+ * @author Bear
+ * @version 1.0
+ * 
+ */
+
+;( function( root, factory ) {
+
+    root.owlcarousel2_filter = factory( root.jQuery );
+
+} ) ( window, function( $, args ) {
+
+    'use strict';
+
+    var OwlCarousel2_Filter = function( filter, $args ) {
+
+        var owl = this;
+        var owl_object = owl.data( 'owl.carousel' );
+        var owl_settings = owl_object.settings;
+
+        // Destroy OwlCarousel 
+        owl.trigger( 'destroy.owl.carousel' );
+        
+        // Clone
+        if( ! owl.oc2_filter_clone )
+            owl.oc2_filter_clone = owl.clone()
+        
+        // Filter elements and clone
+        var clone_filter_items = owl.oc2_filter_clone.children( filter ).clone();
+
+        // Put filter items and re-call OwlCarousel
+        owl.empty().append( clone_filter_items ).owlCarousel( owl_settings )
+    }
+
+    $.fn.owlcarousel2_filter = OwlCarousel2_Filter;
+
+} )
